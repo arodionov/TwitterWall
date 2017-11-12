7169895af25816941f36e4b4177346689debcae9
Andrii_Rodionov <andrii.rodionov@gmail.com>
2017-11-12 10:48:53 +0200

    Decouple TW and Twitter API

--- src/main/kotlin/ua/jug/tw/TwitterWallRunner.kt
+++ src/main/kotlin/ua/jug/tw/TwitterWallRunner.kt
@@ -1,9 +1,10 @@
 package ua.jug.tw
 
 import io.javalin.Javalin
-import twitter4j.Twitter
-import twitter4j.TwitterFactory
-import twitter4j.TwitterStreamFactory
+import twitter4j.*
+import twitter4j.util.function.Consumer
+import ua.jug.buffer.Buffer
+import ua.jug.kotlindemo.TwitterBot
 
 
 fun main(args: Array<String>) {
@@ -12,8 +13,28 @@ fun main(args: Array<String>) {
 
     val tags = listOf("#JavaDayUA", "#JUGUA", "#JavaDayUA2017")
 
-    val twitterWall = TwitterWall(TwitterStreamFactory().instance, tags)
-    twitterWall.fillBuffer { twitterWall.searchTweets(TwitterFactory().instance) }
+    val twitterWall = TwitterWall(
+            twitterMessagesBufferUpdater(TwitterStreamFactory().instance),
+            tags
+    )
+    twitterWall.fillBuffer( searchTweets(TwitterFactory().instance) )
 
     app.get("/tw") { ctx -> ctx.json(twitterWall.tweets()) }
+}
+
+fun twitterMessagesBufferUpdater(twitterStream: TwitterStream) = { buffer: Buffer<String>, hashtags: List<String> ->
+
+    TwitterBot(twitterStream,
+            Consumer { status -> buffer.put(status.text) }
+    ).subscribe(* hashtags.toTypedArray())
+
+}
+
+fun searchTweets(twitter: Twitter) = { hashtags: List<String> ->
+
+    val query = Query(hashtags[0])
+    val result = twitter.search(query)
+    result.tweets
+            .map { status -> status.text }
+
 }
