1746f6d4cdfe0afce5785443f53eee155ebe5759
Andrii_Rodionov <andrii.rodionov@gmail.com>
2017-11-16 10:08:37 +0200

    Reformat test

--- src/test/kotlin/ua/jug/tw/TwitterWallTest.kt
+++ src/test/kotlin/ua/jug/tw/TwitterWallTest.kt
@@ -12,52 +12,52 @@ class TwitterWallTest : FunSpec() {
     init {
 
         val twitter = { _: Buffer<String>, _: List<String> -> Unit }
-        fun twitterWall() = TwitterWall( twitter, listOf("twitterMessagesBufferUpdater"))
+        fun twitterWall() = TwitterWall(twitter, listOf("#JUG"))
 
         test("Create TW") {
-                twitterWall()
-            }
-
-            test("List of tweets") {
-                val twitterWall: TwitterWall = twitterWall()
-                val tweets: List<String> = twitterWall.tweets()
-            }
-
-            test("Sized buffer") {
-                TwitterWall(twitter, listOf("a"), 10)
-            }
-
-            test("Subscribe on hashtags") {
-                val hashtags = listOf("#JavaDayUA", "#JUGUA")
-                TwitterWall(twitter, hashtags = hashtags, size = 20)
-            }
-
-            test("hashtags should not be empty") {
-                val hashtags = listOf<String>()
-                shouldThrow<IllegalArgumentException> {
-                    TwitterWall(twitter, hashtags = hashtags)
-                }.message shouldBe "Empty list"
-            }
-
-            test("hashtags should not be blank") {
-                val hashtags = listOf<String>(" ", "")
-                shouldThrow<IllegalArgumentException> {
-                    TwitterWall(twitter, hashtags = hashtags)
-                }.message shouldBe "Empty list"
-            }
-
-            test("prefill buffer") {
-                val twitterWall = TwitterWall(twitter, listOf("#JavaDayUA2017"), 2)
-
-                twitterWall.fillBuffer{ listOf("a #JavaDayUA2017", "#JavaDayUA2017 b", "#JavaDayUA2017 c") }
-
-                val size = twitterWall.tweets().size
-                size shouldBe gt(0)
-                size shouldBe lte(2)
-                twitterWall.tweets().filter { it.contains("#JavaDayUA2017") }.size shouldBe size
-            }
-
+            twitterWall()
         }
-   }
+
+        test("List of tweets") {
+            val twitterWall: TwitterWall = twitterWall()
+            val tweets: List<String> = twitterWall.tweets()
+        }
+
+        test("Sized buffer") {
+            TwitterWall(twitter, listOf("a"), 10)
+        }
+
+        test("Subscribe on hashtags") {
+            val hashtags = listOf("#JavaDayUA", "#JUGUA")
+            TwitterWall(twitter, hashtags = hashtags, size = 20)
+        }
+
+        test("hashtags should not be empty") {
+            val hashtags = listOf<String>()
+            shouldThrow<IllegalArgumentException> {
+                TwitterWall(twitter, hashtags = hashtags)
+            }.message shouldBe "Empty list"
+        }
+
+        test("hashtags should not be blank") {
+            val hashtags = listOf<String>(" ", "")
+            shouldThrow<IllegalArgumentException> {
+                TwitterWall(twitter, hashtags = hashtags)
+            }.message shouldBe "Empty list"
+        }
+
+        test("prefill buffer") {
+            val twitterWall = TwitterWall(twitter, listOf("#JavaDayUA2017"), 2)
+
+            twitterWall.fillBuffer { listOf("a #JavaDayUA2017", "#JavaDayUA2017 b", "#JavaDayUA2017 c") }
+
+            val size = twitterWall.tweets().size
+            size shouldBe gt(0)
+            size shouldBe lte(2)
+            twitterWall.tweets().filter { it.contains("#JavaDayUA2017") }.size shouldBe size
+        }
+
+    }
+}
 
 
